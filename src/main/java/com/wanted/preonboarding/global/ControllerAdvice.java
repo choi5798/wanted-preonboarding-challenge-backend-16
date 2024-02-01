278dfc20ef1efd3b29eb0b6d2fafe76949ce398f
Choi Seongwon <vlflalektzja@naver.com>
2024-02-01 16:25:22 +0900

    feat: ControllerAdvice 구축

---
+++ src/main/java/com/wanted/preonboarding/global/ControllerAdvice.java
@@ -0,0 +1,28 @@
+package com.wanted.preonboarding.global;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.text.MessageFormat;
+
+@Slf4j
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResponse exceptionHandle(Exception exception) {
+        log.warn(MessageFormat.format("Exception occurred: {0}", exception.getClass().getName()));
+        log.warn(MessageFormat.format("Message: {0}", exception.getMessage()));
+
+        return ErrorResponse.create(
+                exception,
+                HttpStatus.BAD_REQUEST,
+                exception.getMessage());
+    }
+
+}
